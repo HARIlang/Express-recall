aa258aa4bbb0d3c34260620f026a046167f8c017
hari <amarhari26122002@gmail.com>
2026-03-03 03:37:05 +0530

    homme page and contact page has ben created

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+const express = require('express'); 
+const app = express();
+const port = 4000;
+
+app.get('/', (req, res) => {     // home page;
+  res.send("Welcome to home page");
+});
+
+app.get('/contact',(req,res)=>{   // contact page
+res.send("welcome to contact page");
+
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});  // created server using the express
